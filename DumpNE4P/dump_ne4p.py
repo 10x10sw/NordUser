5d6e880c4ebdd0c6fdf07ca9e2d50382e73addfe
Christian E <christian@tenbyten.com>
2016-11-11 17:28:22 -0500

    DumpNE4P improvements and readme

--- DumpNE4P/dump_ne4p.py
+++ DumpNE4P/dump_ne4p.py
@@ -34,16 +34,9 @@ import struct
 import sys
 
 parser = argparse.ArgumentParser(description='Dumps the program settings contained in Nord Electro 4 .ne4p files.')
-parser.add_argument('inputFile', type=str, help='the input Nord Sound Manager ne4p file')
+parser.add_argument('inputFile', type=argparse.FileType('rb'), nargs='+', help='input Nord Sound Manager ne4p file(s)')
 args = parser.parse_args()
 # print(args)
-
-# read the input binary
-d = open(args.inputFile, 'rb').read()
-
-if d[:4] != 'CBIN' and d[9:13] != 'ne4p':
-    print 'This does not appear to be a Nord NE4P file'
-    exit
 
 # function to convert 0x00 -> 0x3f to -15 -> +15
 def dbFromSevenBitValue(i):
@@ -60,167 +53,184 @@ def sevenBitValueFromChars(c1,c2,offset):
     return sbv
 
 
-#ORGAN
+def dumpNE4P(d):
 
-# model
-i = ord(d[0x10])
-organ = ''
-if i==0x12:
-    organ = 'B3'
-if i==0x13:
-    organ = 'Farf'
-if i==0x14:
-    organ = 'Vox'
+    dump = ''
+    
+    ######## ORGAN ########
 
-# 0x49: bit 4: vib/chorus ON
-i = ord(d[0x49])
-if i & 0x0a:
-    vibState = 'On'
-else:
-    vibState = 'Off'
+    # model
+    i = ord(d[0x10])
+    organ = ''
+    if i==0x12:
+        organ = 'B3'
+    if i==0x13:
+        organ = 'Farf'
+    if i==0x14:
+        organ = 'Vox'
 
-# 0x3c-0x3f: drawbars 1-8, 2 drawbars per byte, values 0-8
-# 0x40: drawbar 9 in high byte
-i = ord(d[0x3c])
-drawbar1 = i >> 4
-drawbar2 = i & 0xf
-i = ord(d[0x3d])
-drawbar3 = i >> 4
-drawbar4 = i & 0xf
-i = ord(d[0x3e])
-drawbar5 = i >> 4
-drawbar6 = i & 0xf
-i = ord(d[0x3f])
-drawbar7 = i >> 4
-drawbar8 = i & 0xf
-i = ord(d[0x40])
-drawbar9 = i >> 4
+    # 0x49: bit 4: vib/chorus ON
+    i = ord(d[0x49])
+    if i & 0x0a:
+        vibState = 'On'
+    else:
+        vibState = 'Off'
 
-# 0x40: vibrato/chorus on (b3) 0x00=off 0x01=v1 0x03=c1 0x05=v2
-i = ord(d[0x40]) >> 1
-i = i & 0x7
-print(i)
-vibType = ''
-if i==0:
-    vibType = 'V1'
-if i==1:
-    vibType = 'C1'
-if i==2:
-    vibType = 'V2'
-if i==3:
-    vibType = 'C2'
-if i==4:
-    vibType = 'V3'
-if i==5:
-    vibType = 'C3'
+    # 0x3c-0x3f: drawbars 1-8, 2 drawbars per byte, values 0-8
+    # 0x40: drawbar 9 in high byte
+    i = ord(d[0x3c])
+    drawbar1 = i >> 4
+    drawbar2 = i & 0xf
+    i = ord(d[0x3d])
+    drawbar3 = i >> 4
+    drawbar4 = i & 0xf
+    i = ord(d[0x3e])
+    drawbar5 = i >> 4
+    drawbar6 = i & 0xf
+    i = ord(d[0x3f])
+    drawbar7 = i >> 4
+    drawbar8 = i & 0xf
+    i = ord(d[0x40])
+    drawbar9 = i >> 4
 
-if organ:
-    print 'Organ: {}  Vibrato/Chorus: {}  Type:{}  Drawbars: {}{}{}{}{}{}{}{}{}'.format(organ,vibState,vibType,\
-                                                                               drawbar1,drawbar2,drawbar3,\
-                                                                               drawbar4,drawbar5,drawbar6,\
-                                                                               drawbar7,drawbar8,drawbar9)
+    # 0x40: vibrato/chorus on (b3) 0x00=off 0x01=v1 0x03=c1 0x05=v2
+    i = ord(d[0x40]) >> 1
+    i = i & 0x7
+    vibType = ''
+    if i==0:
+        vibType = 'V1'
+    if i==1:
+        vibType = 'C1'
+    if i==2:
+        vibType = 'V2'
+    if i==3:
+        vibType = 'C2'
+    if i==4:
+        vibType = 'V3'
+    if i==5:
+        vibType = 'C3'
 
+    if organ:
+        dump += 'Organ: {}  Vibrato/Chorus: {}  Type:{}  Drawbars: {}{}{}{}{}{}{}{}{}\n'.format(organ,vibState,vibType,\
+                                                                                   drawbar1,drawbar2,drawbar3,\
+                                                                                   drawbar4,drawbar5,drawbar6,\
+                                                                                   drawbar7,drawbar8,drawbar9)
 
-# EFFECTS
+    ######## EFFECTS ########
 
-# 0x7b: bit 0: EQ ON
-i = ord(d[0x7b])
-if i & 0x80:
-    eqState = 'On'
-else:
-    eqState = 'Off'
+    # 0x7b: bit 0: EQ ON
+    i = ord(d[0x7b])
+    if i & 0x80:
+        eqState = 'On'
+    else:
+        eqState = 'Off'
 
-# 0x5f: EQ Bass Gain (7 bits)
-i = (ord(d[0x5f]) >> 1)
-eqBassGain = dbFromSevenBitValue(i)
+    # 0x5f: EQ Bass Gain (7 bits)
+    i = (ord(d[0x5f]) >> 1)
+    eqBassGain = dbFromSevenBitValue(i)
 
-# 0x5f, 0x60: EQ Mid Freq 0=200 40=1000 7f=8K
-# - - - -  - - - x  x x x x  x x - -
-#msb = ord(d[0x5f]) & 0x1
-#lsb = ord(d[0x60]) >> 2
-#i = ((msb << 6) + lsb)
-i = sevenBitValueFromChars(d[0x5d],d[0x60],7)
-if i<0x40:
-    f = 800.0 * i / 0x3f
-    f += 200
-else:
-    i -= 0x40
-    f = 7000.0 * i / 0x3f
-    f += 1000
-# print '{} {}'.format(i,f)
-eqMidFreq = int(round(f / 10) * 10)
+    # 0x5f, 0x60: EQ Mid Freq 0=200 40=1000 7f=8K
+    # - - - -  - - - x  x x x x  x x - -
+    #msb = ord(d[0x5f]) & 0x1
+    #lsb = ord(d[0x60]) >> 2
+    #i = ((msb << 6) + lsb)
+    i = sevenBitValueFromChars(d[0x5d],d[0x60],7)
+    if i<0x40:
+        f = 800.0 * i / 0x3f
+        f += 200
+    else:
+        i -= 0x40
+        f = 7000.0 * i / 0x3f
+        f += 1000
+    # print '{} {}'.format(i,f)
+    eqMidFreq = int(round(f / 10) * 10)
 
-# 0x60, 0x61: EQ Mid Gain
-# - - - -  - - x x  x x x x  x - - -
-#msb = ord(d[0x60]) & 0x3
-#lsb = ord(d[0x61]) >> 3
-#i = ((msb << 5) + lsb)
-i = sevenBitValueFromChars(d[0x60],d[0x61],6)
-eqMidGain = dbFromSevenBitValue(i)
+    # 0x60, 0x61: EQ Mid Gain
+    # - - - -  - - x x  x x x x  x - - -
+    #msb = ord(d[0x60]) & 0x3
+    #lsb = ord(d[0x61]) >> 3
+    #i = ((msb << 5) + lsb)
+    i = sevenBitValueFromChars(d[0x60],d[0x61],6)
+    eqMidGain = dbFromSevenBitValue(i)
 
-# 0x61, 0x62: EQ Treble Gain (7 bits)
-# - - - -  - x x x  x x x x  - - - -
-#msb = ord(d[0x61]) & 0x7
-#lsb = ord(d[0x62]) >> 4
-#i = ((msb << 4) + lsb)
-i = sevenBitValueFromChars(d[0x61],d[0x62],5)
-eqTrebleGain = dbFromSevenBitValue(i)
+    # 0x61, 0x62: EQ Treble Gain (7 bits)
+    # - - - -  - x x x  x x x x  - - - -
+    #msb = ord(d[0x61]) & 0x7
+    #lsb = ord(d[0x62]) >> 4
+    #i = ((msb << 4) + lsb)
+    i = sevenBitValueFromChars(d[0x61],d[0x62],5)
+    eqTrebleGain = dbFromSevenBitValue(i)
 
-print 'EQ: {}  Bass:{:+} dB  Mid:{:+} dB ({} Hz)  Treble:{:+} dB'.format(eqState,eqBassGain,eqMidGain,eqMidFreq,eqTrebleGain)
+    dump += 'EQ: {}  Bass:{:+} dB  Mid:{:+} dB ({} Hz)  Treble:{:+} dB\n'.format(eqState,eqBassGain,eqMidGain,eqMidFreq,eqTrebleGain)
 
-# 0x67, 0x68: Program Gain (7 bits)
-# - - - -  - - x x  x x x x  x - - -
-#msb = ord(d[0x67]) & 0x3
-#lsb = ord(d[0x68]) >> 3
-#i = ((msb << 5) + lsb) << 1
-i = sevenBitValueFromChars(d[0x67],d[0x68],6)
-programGain = 10. * i / 0x7e
-print 'Program Gain: {}'.format(round(programGain,1))
+    # 0x67, 0x68: Program Gain (7 bits)
+    # - - - -  - - x x  x x x x  x - - -
+    #msb = ord(d[0x67]) & 0x3
+    #lsb = ord(d[0x68]) >> 3
+    #i = ((msb << 5) + lsb) << 1
+    i = sevenBitValueFromChars(d[0x67],d[0x68],6)
+    programGain = 10. * i / 0x7e
+    dump += 'Program Gain: {}\n'.format(round(programGain,1))
 
-# 0x7b bit 1: efx1 on
-i = ord(d[0x7b])
-if i & 0x40:
-    efx1State = 'On'
-else:
-    efx1State = 'Off'
+    # 0x7b bit 1: efx1 on
+    i = ord(d[0x7b])
+    if i & 0x40:
+        efx1State = 'On'
+    else:
+        efx1State = 'Off'
 
-# 0x62, 0x63: EFX1 gain (7 bits) offset 4
-i = sevenBitValueFromChars(d[0x62],d[0x63],4)
-rate = 10. * i / 0x7f
-print 'Effect 1: {}  Rate: {}'.format(efx1State,round(rate,1))
+    # 0x62, 0x63: EFX1 gain (7 bits) offset 4
+    i = sevenBitValueFromChars(d[0x62],d[0x63],4)
+    rate = 10. * i / 0x7f
+    dump += 'Effect 1: {}  Rate: {}\n'.format(efx1State,round(rate,1))
 
-# 0x7b bit 2: efx2 on
-i = ord(d[0x7b])
-if i & 0x20:
-    efx2State = 'On'
-else:
-    efx2State = 'Off'
+    # 0x7b bit 2: efx2 on
+    i = ord(d[0x7b])
+    if i & 0x20:
+        efx2State = 'On'
+    else:
+        efx2State = 'Off'
 
-# 0x63, 0x64: EFX2 gain (7 bits) offset 7
-i = sevenBitValueFromChars(d[0x63],d[0x64],7)
-rate = 10. * i / 0x7f
-print 'Effect 2: {}  Rate: {}'.format(efx2State,round(rate,1))
+    # 0x63, 0x64: EFX2 gain (7 bits) offset 7
+    i = sevenBitValueFromChars(d[0x63],d[0x64],7)
+    rate = 10. * i / 0x7f
+    dump += 'Effect 2: {}  Rate: {}\n'.format(efx2State,round(rate,1))
 
-# 0x7b bit 3: spkr on
-i = ord(d[0x7b])
-if i & 0x10:
-    speakerState = 'On'
-else:
-    speakerState = 'Off'
+    # 0x7b bit 3: spkr on
+    i = ord(d[0x7b])
+    if i & 0x10:
+        speakerState = 'On'
+    else:
+        speakerState = 'Off'
 
-# 0x65, 0x66: Speaker Drive/Comp  (7 bits) offset 2
-i = sevenBitValueFromChars(d[0x65],d[0x66],2)
-rate = 10. * i / 0x7f
-print 'Speaker: {}  Drive/Comp: {}'.format(speakerState,round(rate,1))
+    # 0x65, 0x66: Speaker Drive/Comp  (7 bits) offset 2
+    i = sevenBitValueFromChars(d[0x65],d[0x66],2)
+    rate = 10. * i / 0x7f
+    dump += 'Speaker: {}  Drive/Comp: {}\n'.format(speakerState,round(rate,1))
 
-# 0x7b bit 4: delay/reverb on
-i = ord(d[0x7b])
-if i & 0x08:
-    reverbState = 'On'
-else:
-    reverbState = 'Off'
+    # 0x7b bit 4: delay/reverb on
+    i = ord(d[0x7b])
+    if i & 0x08:
+        reverbState = 'On'
+    else:
+        reverbState = 'Off'
 
-# 0x66, 0x67: Delay/Reverb Mix (7 bits) offset 4
-i = sevenBitValueFromChars(d[0x66],d[0x67],4)
-rate = 10. * i / 0x7f
-print 'Delay/Reverb: {}  Mix: {}'.format(reverbState,round(rate,1))
+    # 0x66, 0x67: Delay/Reverb Mix (7 bits) offset 4
+    i = sevenBitValueFromChars(d[0x66],d[0x67],4)
+    rate = 10. * i / 0x7f
+    dump += 'Delay/Reverb: {}  Mix: {}\n'.format(reverbState,round(rate,1))
+
+    return dump
+
+for f in args.inputFile:
+
+    # read the input binary
+    d = f.read()
+
+    if d[:4] != 'CBIN' and d[9:13] != 'ne4p':
+        print 'This does not appear to be a Nord NE4P file'
+        continue
+    else:
+        print(f.name)
+        print(dumpNE4P(d))
+
